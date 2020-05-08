76d6b383df0732a1e8b9f6217e190aa8c985c4c1
PercivalN <percival.ngan@gmail.com>
2020-05-08 10:24:45 -0500

    Finished ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,26 @@
-def get_indices_of_item_weights(weights, length, limit):
+from hashtable import (HashTable,
+                       put,
+                       get)
 
-    """
-    YOUR CODE HERE
-    """
+def get_indices_of_item_weights(weights, length, limit):
+    ht = HashTable(16)
+
+    for key, value in enumerate(weights):
+        put(ht, value, key)
+
+    for key, value in enumerate(weights):
+        if get(ht, limit - value) is not None:
+            index = get(ht, limit - value)
+
+            if index >= key:
+                return [index, key]
+            else:
+                return [key, index]
 
     return None
+
+def print_answer(answer):
+    if answer is not None:
+        print(str(answer[0] + " " + answer[1]))
+    else:
+        print("None")
